9f8698cdf8a49dbfeb59d8f1a97b3ce2eac71913
武田怜士 <ri76joy@gmail.com>
2020-03-21 12:41:36 +0900

    簡易なメニューの追加など

--- LightNote/main.cs
+++ LightNote/main.cs
@@ -37,6 +37,8 @@ namespace LightNote
         private List<TextPage> pages = new List<TextPage>();
         private int max_index = -1;
 
+        private MenuStrip menu = new MenuStrip();
+
         public LightNote()
         {
             this.SetDesktopBounds(100, 50, 450, 400);
@@ -46,13 +48,32 @@ namespace LightNote
             this.createPage();
 
             panel.Parent = this;
+            panel.Dock = DockStyle.Fill;
             panel.FlowDirection = FlowDirection.TopDown;
+            panel.Controls.Add(this.menu);
             panel.Controls.Add(this.tab);
 
+            #region Menu
+            ToolStripMenuItem
+                m_file = new ToolStripMenuItem("ファイル(F)"),
+                m_edit = new ToolStripMenuItem("編集(E)"),
+                m_view = new ToolStripMenuItem("表示(V)");
+
+            m_file.ShortcutKeys = Keys.Alt | Keys.F;
+            m_edit.ShortcutKeys = Keys.Alt | Keys.E;
+            m_view.ShortcutKeys = Keys.Alt | Keys.V;
+
+            this.menu.Items.Add(m_file);
+            this.menu.Items.Add(m_edit);
+            this.menu.Items.Add(m_view);
+            #endregion
+
+            #region Event
             this.SizeChanged += (object sender, EventArgs e) =>
             {
                 this.setSize();
             };
+            #endregion
         }
     }
 }
