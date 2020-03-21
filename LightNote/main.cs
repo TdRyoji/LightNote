2166fefd64c90c46960b246fd0aee1d8c8a8fe6f
武田怜士 <ri76joy@gmail.com>
2020-03-21 19:51:39 +0900

    menuの設定

--- LightNote/main.cs
+++ LightNote/main.cs
@@ -37,6 +37,11 @@ namespace LightNote
         private int max_index = -1;
 
         private MenuStrip menu = new MenuStrip();
+        private ToolStripMenuItem
+            m_new, m_open, m_close, m_closeNew, m_closeOpen,
+            m_save, m_saveAs, m_saveAll, m_exit,
+            m_undo, m_redo, m_cut, m_copy, m_paste, m_delete,
+            m_font, m_color, m_justL, m_just, m_justR;
         private ToolStrip tool = new ToolStrip();
         private ToolStripButton
             b_new, b_open, b_save, b_saveAll, b_print, b_close,
@@ -63,10 +68,9 @@ namespace LightNote
             panel.Controls.Add(this.tool);
             panel.Controls.Add(this.tab);
             #endregion
-
-            #region Image
+            #region Images
             i_new = Properties.Resources.newfile;
-            i_open = Properties.Resources.close;
+            i_open = Properties.Resources.open;
             i_save = Properties.Resources.save;
             i_saveAll = Properties.Resources.saveall;
             i_print = Properties.Resources.print;
@@ -85,18 +89,192 @@ namespace LightNote
             #endregion
 
             #region Menu
+            #region Topitems
             ToolStripMenuItem
                 m_file = new ToolStripMenuItem("ファイル(F)"),
                 m_edit = new ToolStripMenuItem("編集(E)"),
-                m_view = new ToolStripMenuItem("表示(V)");
-
-            m_file.ShortcutKeys = Keys.Alt | Keys.F;
-            m_edit.ShortcutKeys = Keys.Alt | Keys.E;
-            m_view.ShortcutKeys = Keys.Alt | Keys.V;
+                m_option = new ToolStripMenuItem("書式(O)");
 
             this.menu.Items.Add(m_file);
             this.menu.Items.Add(m_edit);
-            this.menu.Items.Add(m_view);
+            this.menu.Items.Add(m_option);
+
+            m_file.ShortcutKeys = Keys.Alt | Keys.F;
+            m_edit.ShortcutKeys = Keys.Alt | Keys.E;
+            m_option.ShortcutKeys = Keys.Alt | Keys.O;
+            #endregion
+
+            #region File
+            #region Declaration
+            m_new = new ToolStripMenuItem("新規作成", this.i_new);
+            m_open = new ToolStripMenuItem("開く", this.i_open);
+            m_close = new ToolStripMenuItem("閉じる", this.i_close);
+            m_closeNew = new ToolStripMenuItem("閉じて新規作成");
+            m_closeOpen = new ToolStripMenuItem("閉じて開く");
+            m_save = new ToolStripMenuItem("現在のテキストファイルを上書き保存", this.i_save);
+            m_saveAs = new ToolStripMenuItem("現在のテキストファイルに名前を付けて保存");
+            m_saveAll = new ToolStripMenuItem("全て保存", this.i_saveAll);
+            m_exit = new ToolStripMenuItem("終了");
+
+            m_file.DropDownItems.Add(m_new);
+            m_file.DropDownItems.Add(m_open);
+            m_file.DropDownItems.Add(new ToolStripSeparator());
+            m_file.DropDownItems.Add(m_close);
+            m_file.DropDownItems.Add(m_closeNew);
+            m_file.DropDownItems.Add(m_closeOpen);
+            m_file.DropDownItems.Add(new ToolStripSeparator());
+            m_file.DropDownItems.Add(m_save);
+            m_file.DropDownItems.Add(m_saveAs);
+            m_file.DropDownItems.Add(m_saveAll);
+            m_file.DropDownItems.Add(new ToolStripSeparator());
+            m_file.DropDownItems.Add(m_exit);
+            #endregion
+            #region ShortcutKeys and Events
+            m_new.ShortcutKeys = Keys.Control | Keys.N;
+            m_open.ShortcutKeys = Keys.Control | Keys.O;
+            m_close.ShortcutKeys = Keys.Alt | Keys.C;
+            m_closeNew.ShortcutKeys = Keys.Alt | Keys.R;
+            m_closeOpen.ShortcutKeys = Keys.Alt | Keys.O;
+            m_save.ShortcutKeys = Keys.Control | Keys.S;
+            m_saveAs.ShortcutKeys = Keys.Control | Keys.A;
+            m_saveAll.ShortcutKeys = Keys.Control | Keys.L;
+            m_exit.ShortcutKeys = Keys.Alt | Keys.X;
+
+            m_new.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_open.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_close.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_closeNew.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_closeOpen.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_save.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_saveAs.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_saveAll.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_exit.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            #endregion
+            #endregion
+
+            #region Edit
+            #region Declaration
+            m_undo = new ToolStripMenuItem("元に戻す", this.i_undo);
+            m_redo = new ToolStripMenuItem("やり直し", this.i_redo);
+            m_cut = new ToolStripMenuItem("切り取り", this.i_cut);
+            m_copy = new ToolStripMenuItem("コピー", this.i_copy);
+            m_paste = new ToolStripMenuItem("貼り付け", this.i_paste);
+            m_delete = new ToolStripMenuItem("削除", this.i_delete);
+
+            m_edit.DropDownItems.Add(m_undo);
+            m_edit.DropDownItems.Add(m_redo);
+            m_edit.DropDownItems.Add(new ToolStripSeparator());
+            m_edit.DropDownItems.Add(m_cut);
+            m_edit.DropDownItems.Add(m_copy);
+            m_edit.DropDownItems.Add(m_paste);
+            m_edit.DropDownItems.Add(m_delete);
+            #endregion
+            #region ShortcutKeys and Events
+            m_undo.ShortcutKeys = Keys.Control | Keys.Z;
+            m_redo.ShortcutKeys = Keys.Control | Keys.Y;
+            m_cut.ShortcutKeys = Keys.Control | Keys.X;
+            m_copy.ShortcutKeys = Keys.Control | Keys.C;
+            m_paste.ShortcutKeys = Keys.Control | Keys.V;
+            m_delete.ShortcutKeys = Keys.Delete;
+
+            m_undo.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_redo.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_cut.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_copy.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_paste.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_delete.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            #endregion
+            #endregion
+
+            #region Option
+            #region Declaration
+            m_font = new ToolStripMenuItem("フォント", this.i_font);
+            m_color = new ToolStripMenuItem("カラー", this.i_color);
+            m_justL = new ToolStripMenuItem("左揃え", this.i_justL);
+            m_just = new ToolStripMenuItem("中央揃え", this.i_just);
+            m_justR = new ToolStripMenuItem("右揃え", this.i_justR);
+
+            m_option.DropDownItems.Add(m_font);
+            m_option.DropDownItems.Add(m_color);
+            m_option.DropDownItems.Add(new ToolStripSeparator());
+            m_option.DropDownItems.Add(m_justL);
+            m_option.DropDownItems.Add(m_just);
+            m_option.DropDownItems.Add(m_justR);
+            #endregion
+            #region ShortcutKeys and Events
+            m_font.ShortcutKeys = Keys.Control | Keys.F1;
+            m_color.ShortcutKeys = Keys.Control | Keys.F2;
+            m_justL.ShortcutKeys = Keys.Control | Keys.Shift | Keys.L;
+            m_just.ShortcutKeys = Keys.Control | Keys.Shift | Keys.J;
+            m_justR.ShortcutKeys = Keys.Control | Keys.Shift | Keys.R;
+
+            m_font.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_color.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_justL.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_just.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            m_justR.Click += (object sender, EventArgs e) =>
+            {
+
+            };
+            #endregion
+            #endregion
             #endregion
 
             #region Tool
@@ -141,7 +319,7 @@ namespace LightNote
             this.tool.Items.Add(b_just);
             this.tool.Items.Add(b_justR);
             #endregion
-            #region Text and Event
+            #region Text and Events
             b_new.ToolTipText = "新規作成 (Ctrl+N)";
             b_new.Click += (object sender, EventArgs e) =>
             {
